3baea296b26071c3908a7dc43cda56f554643bdc
Nick Yee <njsyee@gmail.com>
2016-02-09 15:52:03 -0700

    Got json data to display.

--- app/actions.rb
+++ app/actions.rb
@@ -3,6 +3,10 @@ get '/' do
   erb :index
 end
 
-get '/nauts/?' do
+get '/nauts' do
   Naut.all.to_json
+end
+
+get '/selected_naut' do
+  Naut.first.to_json
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,10 +1,22 @@
+'use strict';
+
 $(document).ready(function() {
 
   (function() {
-    var getNauts = function(data) {
-      console.log(data);
+    var getNauts = function(nauts) {
+      console.log(nauts);
     };
 
     $.getJSON('/nauts', getNauts);
   })();
+
+  (function() {
+    var selected = function(naut) {
+      $('#name').text(naut.first_name + " " + naut.last_name)
+    };
+
+    $.getJSON('/selected_naut', selected);
+  })();
+
+
 });
