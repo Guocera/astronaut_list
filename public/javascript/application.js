f91e89509d78e86fe4e23886aca6a0da23c69565
Nick Yee <njsyee@gmail.com>
2016-02-09 18:53:47 -0700

    Loads all the naut images.

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -3,17 +3,20 @@
 $(document).ready(function() {
 
   var getNauts = function(nauts) {
-    $('#nauts-list').append($("<div></div>")
-      .addClass("col-sm-3 col-xs-6")
-      .append($("<a></a>")
-        .attr({ href: "#" })
-        .append($("<img></img>")
-          .addClass("img-responsive portfolio-item")
-          .attr({ src: "http://placehold.it/500x300" })
-          .attr({ alt: "" })
+    for(var i = 0; i < nauts.length; i++) {
+      $('#nauts-list').append($("<div></div>")
+        .addClass("col-sm-3 col-xs-6")
+        .append($("<a></a>")
+          .attr({ href: "#" })
+          .append($("<img></img>")
+            .addClass("img-responsive portfolio-item")
+            .attr({ src: "http://lorempixel.com/500/300/" })
+            .attr({ alt: "" })
+          )
         )
-      )
-    );
+      );  
+    }
+    
   };
 
   var selected = function(naut) {
